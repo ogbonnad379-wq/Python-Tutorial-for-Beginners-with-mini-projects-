ec1116480c9c8cc228ad973c4eeaccad9c3e1ee5
David <ogbonnad379@gmail.com>
2025-10-15 11:58:06 +0100

    added LESSON12

---
+++ LESSON12/modules.py
@@ -0,0 +1,21 @@
+
+
+from math import pi
+import sys
+import random as rdm
+from enum import Enum
+import Kansas
+from rps7 import rock_paper_scissors
+
+print(pi)
+
+# for item in dir(rdm):
+#     print(item)
+
+print(Kansas.capital)
+Kansas.randomfunfact()
+
+print(__name__)
+print(Kansas.__name__)
+
+rock_paper_scissors()
